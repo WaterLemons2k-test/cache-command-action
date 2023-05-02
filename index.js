52833ffba8a97cc0fecc90a14611fe6b10717c0a
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-02 18:41:15 +0800

    Set key to path

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const cache = require("@actions/cache")
 const fs = require('fs')
 
-const paths = ['hello']
-const key = 'npm-foobar-d5ea0750'
-fs.writeFile('hello', 'hello', err => {
+const path = '.cacheCommand'
+const key = path
+fs.writeFile(path, path, err => {
   if (err) {
     console.error(err);
   }
-  // file written successfully
 });
 async function getCache() {
-    const cacheId = await cache.restoreCache(paths, key)
+    const cacheId = await cache.restoreCache([path], key)
     if (cacheId) {
         console.log("hit: true")
         return;
     }
-    await cache.saveCache(paths, key)
+    await cache.saveCache([path], key)
 }
 
 getCache()
