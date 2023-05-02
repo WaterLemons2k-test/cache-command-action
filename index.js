fe0d8974abbbb951e5cea92af2f168fae03f5bdc
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-02 21:08:36 +0800

    fix: set command to required

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const core = require("@actions/core")
 const exec = require('@actions/exec');
 const fs = require('fs')
 
-const path = core.getInput('path')
-const command = core.getInput('run')
+const path = core.getInput('path');
+const command = core.getInput('run', { require: true });
+
+// if (!path === null || !command)
+
 let output;
 const options = {};
 options.listeners = {
