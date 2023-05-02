d9d172f60138590f6a82bfb553aa44c6a20bb0a5
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-02 22:15:46 +0800

    test: no input run

--- index.js
+++ index.js
@@ -8,35 +8,35 @@ async function run() {
     const path = core.getInput('path');
     const command = core.getInput('run', { required: true });
 
-  let output;
-  const options = {};
-  options.listeners = {
-    stdout: (data) => {
-      output = data.toString();
+    let output;
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        output = data.toString();
+      }
+    };
+
+    await exec.exec(command, [], options);
+
+    core.setOutput('output', output)
+
+    await fs.writeFile(path, output, err => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+
+    const cacheId = await cache.restoreCache([path], output)
+    if (!cacheId) {
+      // Cache not restored
+      await cache.saveCache([path], output)
+      core.setOutput("cache-hit", false)
+      return;
     }
-  };
 
-  await exec.exec(command, [], options);
-
-  core.setOutput('output', output)
-
-  await fs.writeFile(path, output, err => {
-    if (err) {
-      console.error(err);
-    }
-  });
-
-  const cacheId = await cache.restoreCache([path], output)
-  if (!cacheId) {
-    // Cache not restored
-    await cache.saveCache([path], output)
-    core.setOutput("cache-hit", false)
-    return;
-  }
-
-  core.setOutput("cache-hit", true)
+    core.setOutput("cache-hit", true)
   } catch (error) {
-      core.setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
 
