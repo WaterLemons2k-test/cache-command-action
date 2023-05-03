6a573960077576baaa70414d478cd4a8837c52e9
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-03 12:47:12 +0800

    try catch nesting

--- index.js
+++ index.js
@@ -9,40 +9,44 @@ async function run() {
         if (!file) throw new Error(`Input not supplied: file`);
         const command = core.getInput('run', { required: true });
 
-        let output = '';
-        await exec.exec(command, [], {
-          listeners: {
-            stdout: (data) => {output = data.toString();}
-          }
-        });
-        core.setOutput('output', output)
-    } catch (error) {
-        core.setOutput('hit', false)
-        core.setFailed(`Run command failed: ${error.message}`)
-        return;
-    }
-
-    await fs.writeFile(file, output, err => {
-        if (err) {
-            core.setFailed(`Write command output to ${file} failed: ${err}`);
-            process.exit();
-        }
-    });
-
-    try {
-        const cacheId = await cache.restoreCache([file], output)
-        if (!cacheId) {
-          // Cache not restored
-          await cache.saveCache([file], output)
-          core.setOutput("hit", false)
+        try {
+          let output = '';
+          core.info(`Starting to run command ${command}`)
+          await exec.exec(command, [], {
+            listeners: {
+              stdout: (data) => {output = data.toString();}
+            }
+          });
+          core.setOutput('output', output)
+        } catch (error) {
+          core.setFailed(`Run command failed: ${error.message}`)
           return;
         }
 
-        // Cache restored
-        core.setOutput("hit", true)
+        await fs.writeFile(file, output, err => {
+            if (err) {
+                core.setFailed(`Write command output to ${file} failed: ${err}`);
+                process.exit();
+            }
+        });
+
+        try {
+          const cacheId = await cache.restoreCache([file], output)
+          if (!cacheId) {
+            // Cache not restored
+            await cache.saveCache([file], output)
+            core.setOutput("hit", false)
+            return;
+          }
+
+          // Cache restored
+          core.setOutput("hit", true)
+        } catch (error) {
+          core.setFailed(`Cache failed: ${error.message}`)
+        }
     } catch (error) {
       core.setOutput('hit', false)
-      core.setFailed(`Cache failed: ${error.message}`)
+      core.setFailed(`${error.message}`)
     }
 }
 
