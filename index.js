1ad57021dc251bc99e3e1447884d4a04919703d6
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-03 16:15:30 +0800

    re-use exec again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cache = require("@actions/cache")
 const core = require("@actions/core")
-const { spawn } = require('child_process');
+const exec = require('@actions/exec');
 const fs = require('fs')
 
 async function run() {
@@ -8,23 +8,18 @@ async function run() {
         const file = core.getInput('file');
         if (!file) throw new Error(`Input not supplied: file`);
         const command = core.getInput('run', { required: true });
-     
-        // Write command to Shell script
-        const script = './.cacheCommand.sh'
-        await fs.writeFile(script, '#!/bin/bash\n' + command, err => {
-            if (err) {
-                core.setFailed(`Write command to Shell script failed: ${err}`);
-                process.exit();
-            }
-        });
 
           let output = '';
           core.info(`Starting to run command ${command}`)
-          await spawn('bash', [script]).stdout.on('data', (data) => {
-            output += data.toString();
-            console.log('output: ', output);
-            core.setOutput('output', output)
+          await exec.exec(command, [], {
+            listeners: {
+              stdout: (data) => {
+                output += data.toString().replace(/\n/g, ' ');
+                console.log("output: ", output);
+              }
+            }
           });
+          core.setOutput('output', output)
 
         await fs.writeFile(file, output, err => {
             if (err) {
