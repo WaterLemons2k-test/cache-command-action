fd9563bfe74bdd35a54a332ed02082f0105254d1
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-16 19:17:48 +0800

    fix: `replaceLF`

--- src/log.ts
+++ src/log.ts
@@ -1,9 +1,15 @@
 import { setFailed, setOutput } from '@actions/core';
 
+// replaceLF replace Line feed to an URL encoded character.
+// https://www.eso.org/~ndelmott/url_encode.html
+const replaceLF = (s: string) => {
+  return s.replace(/\n/g, '%0A');
+}
+
 // debug setting a debug message.
 // https://docs.github.com/actions/using-workflows/workflow-commands-for-github-actions#setting-a-debug-message
 export const debug = (message: string) => {
-  console.log(`::debug::${message}`);
+  console.log(`::debug::${replaceLF(message)}`);
 }
 
 // failed sets all outputs to false when catching err,
