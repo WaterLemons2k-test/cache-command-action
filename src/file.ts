e53dfc4f8fdde52c1419f5ff473480b0db507cb1
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-07-06 13:12:38 +0800

    feat: `prepareKeyValue`

--- src/file.ts
+++ src/file.ts
@@ -1,6 +1,7 @@
 import { debug, failed } from './log';
 import { appendFileSync, closeSync, openSync, unlink } from 'node:fs';
 import { EOL } from 'node:os';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Create a empty file
@@ -33,19 +34,42 @@ export const deleteFile = (file: string) => {
 export const setOutput = (key: string, value: unknown) => {
   const filePath = process.env['GITHUB_OUTPUT'] || '';
 
-  // Converted value to a string.
-  value = toStringValue(value);
-
   if (filePath) {
-    debug(`Set output: ${key}=${value}`);
-    appendFileSync(filePath, `${key}=${value}${EOL}`, {
+    debug(`Set output: ${prepareKeyValue(key, value)}`);
+    appendFileSync(filePath, prepareKeyValue(key, value), {
       encoding: 'utf8'
     });
   }
 };
 
+// https://github.com/actions/toolkit/blob/main/packages/core/src/file-command.ts#L27
+const prepareKeyValue = (key: string, value: unknown): string => {
+  const delimiter = `ghadelimiter_${uuidv4()}`;
+
+  // Converted value to a string.
+  const convertedValue = toStringValue(value);
+
+  // These should realistically never happen, but just in case someone finds a
+  // way to exploit uuid generation let's not allow keys or values that contain
+  // the delimiter.
+  if (key.includes(delimiter)) {
+    throw new Error(
+      `Unexpected input: name should not contain the delimiter "${delimiter}"`
+    );
+  }
+
+  if (convertedValue.includes(delimiter)) {
+    throw new Error(
+      `Unexpected input: value should not contain the delimiter "${delimiter}"`
+    );
+  }
+
+  return `${key}<<${delimiter}${EOL}${convertedValue}${EOL}${delimiter}`;
+};
+
 /**
  * Sanitizes an value into a string
+ * https://github.com/actions/toolkit/blob/main/packages/core/src/utils.ts#L11
  * @param value value to sanitize into a string
  * @returns string sterilized
  */
