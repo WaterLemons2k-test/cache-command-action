52ea4389868614bc9f845c27d840d41da9658582
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-07-07 11:33:47 +0800

    feat!: change the default file to `.cache-command-action-file`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { isCacheFound } from './actions/cache';
 import { getCommandOutput } from './actions/command';
-import { error, getInput, setOutput } from '@actions/core';
+import { getInput, setFailed, setOutput } from '@actions/core';
 
 const run = async () => {
   // Getting the output of the input run
@@ -8,21 +8,10 @@ const run = async () => {
   setOutput('output', output);
 
   // Cache file used as a placeholder
-  const file = '.cache';
+  const file = '.cache-command-action-file';
 
   // Set the output hit depending on whether the cache is found or not
   setOutput('hit', await isCacheFound(file, output));
 };
 
-run().catch((err) => {
-  // Make sure the outputs are null or false.
-  setOutput('output', '');
-  setOutput('hit', false);
-
-  // If err not an Error, throw it.
-  // https://stackoverflow.com/a/70993058
-  if (!(err instanceof Error)) throw err;
-
-  process.exitCode = 1;
-  error(err.message);
-});
+run().catch(err => setFailed(err.message));
